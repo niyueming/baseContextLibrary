d6f3a4622645a584b7e9303c46b905539e9e26e1
niyueming <niyueming@163.com>
2017-12-12 08:37:32 +0800

    新增

---
+++ src/main/java/net/nym/basecontextlibrary/widget/ScrollViewPager.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017  Ni YueMing<niyueming@163.com>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package net.nym.basecontextlibrary.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * @author niyueming
+ * @date 2017-12-08
+ * @time 13:35
+ */
+
+public class ScrollViewPager extends ViewPager {
+    private boolean isCanScroll = true;
+    public ScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public ScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 设置其是否能滑动换页
+     * @param isCanScroll false 不能换页， true 可以滑动换页
+     */
+    public void setCanScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return isCanScroll && super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return isCanScroll && super.onTouchEvent(ev);
+
+    }
+}
